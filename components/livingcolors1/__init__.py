897cf77413cc486dc89e27be9fcd239fefb30345
Rogier van Staveren <rogier@batoid.com>
2024-04-16 06:47:59 +0200

    Fix import

--- components/livingcolors1/__init__.py
+++ components/livingcolors1/__init__.py
@@ -1,13 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-import esphome.components.cc2500 as cc2500
+from esphome.components.cc2500 import CC2500Device, CC2500_DEVICE_SCHEMA
 
 DEPENDENCIES = ["cc2500"]
 CODEOWNERS = ["@rrooggiieerr"]
 
 livingcolors1_ns = cg.esphome_ns.namespace("livingcolors1")
-LivingColors1Component = livingcolors1_ns.class_("LivingColors1Component", cc2500.CC2500Device, cg.Component)
+LivingColors1Component = livingcolors1_ns.class_("LivingColors1Component", CC2500Device, cg.Component)
 
 CONF_LIVINGCOLORS1_ID = 'livingcolors1_id'
 # CONF_COMMAND_REPEATS = 'command_repeats'
@@ -16,7 +16,7 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(LivingColors1Component),
     }
-).extend(cc2500.CC2500_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+).extend(CC2500_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
